93759852aa0b3b9231702db70414ba2d053a8bf8
Saad Mukhtar <saadmukhtar01@gmail.com>
2021-09-07 20:08:32 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-function App() {
+const QUERY = gql`
+  {
+    launchesPast(limit: 109) {
+      id
+      mission_name
+      launch_year
+      launch_date_utc
+      rocket {
+        rocket_name
+      }
+    }
+  }
+`;
+
+export default function App() {
+  const { data, loading, error } = useQuery(QUERY);
+  if (data) console.log(data);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>SpaceX Launches</h1>
+        <h2>Past 10 Launches</h2>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Mission Name</th>
+              <th scope="col">Launch Year</th>
+              <th scope="col">Rocket Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.launchesPast.map((launch) => (
+              <tr className="table-dark" key={launch.id}>
+                <td>{launch["id"]}</td>
+                <td>{launch["mission_name"]}</td>
+                <td>{launch["launch_year"]}</td>
+                <td>{launch["rocket"]["rocket_name"]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
-
-export default App;
